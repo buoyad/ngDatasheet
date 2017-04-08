63e15389b61ac29a1280f48e13acdae5c2ee3260
Daniel Ayoub <danny.a95@gmail.com>
2017-04-08 16:54:29 -0400

    Version 0.0.4
    * Added ability to select-highlight cells

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.4
+
+* Added ability to select-highlight cells
--- src/ngdatasheet.component.ts
+++ src/ngdatasheet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, Inject, forwardRef, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
 
 const TAB_KEY = 9;
 const ENTER_KEY = 13;
@@ -7,11 +9,33 @@ const LEFT_KEY = 37;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 const DELETE_KEY = 46;
+const CTRL_KEY = 22;
 
 export enum HEADERS {
   top,
   side,
   both
+}
+
+export class CoordinateMap {
+  [propName: number]: Array<number>;
+
+  public add(i: number, j: number): void {
+    if (!this.contains(i, j)) {
+      if (!this[i]) this[i] = new Array<number>();
+      this[i].push(j);
+    }
+  }
+
+  public contains(i: number, j: number): boolean {
+    return this[i] && this[i].indexOf(j) !== -1;
+  }
+
+  public clear(): void {
+    for (let x of Object.keys(this)) {
+      if (typeof this[<any>x] === 'object') delete this[<any>x];
+    }
+  }
 }
 
 @Component({
@@ -23,8 +47,8 @@ export enum HEADERS {
     <ds-header *ngFor="let index of _w" [top]="true" [index]="index+1"></ds-header>
   </tr>  
   <tr *ngFor="let index of _h; let i = index;">
-    <ds-header [side]="true" [index]="index"></ds-header>
-    <td [ngStyle]="{'text-align': alignment(_data[i][wi])}" *ngFor="let wi of _w">{{ _data[i][wi] }}</td>
+    <ds-header [side]="true" [index]="index+1"></ds-header>
+    <td (mouseover)="onHover($event, i, j)" (mousedown)="beginSelect($event, i, j)" [ngStyle]="{'text-align': alignment(_data[i][j])}" *ngFor="let j of _w" [ngClass]="{'selected': isSelected(i, j)}">{{ _data[i][j] }}</td>
   </tr>
 </table>
 `,
@@ -33,7 +57,14 @@ export enum HEADERS {
       border-collapse: collapse;
       table-layout: fixed;
       font-family: sans-serif;
+      cursor: cell;
     }
+
+    table td.selected, >>> th.selected {
+      border: 1px solid #446CB3;
+      border-style: double;
+      box-shadow: inset 0 -100px 0 rgba(33,133,208,.15);
+    } 
 
     table, td, ds-header {      
       border: 1px solid #ececec;
@@ -98,14 +129,62 @@ export class NgDatasheetComponent implements OnInit {
 
   public nm: Map<number, string | number>;
 
+  public selected: CoordinateMap = new CoordinateMap();
+
+  private _isSelecting: boolean = false;
+  private _startX: number; 
+  private _startY: number;
+
   ngOnInit(): void {
     this._w = Array(this.width).fill(null).map((x, i) => i);
     this._h = Array(this.height).fill(null).map((x, i) => i);
   }
 
+  public fillSelection(x1: number, y1: number, x2: number, y2: number) {
+    this.selected.clear();
+    if (x1 > x2) {
+      [x1, x2] = [x2, x1];
+    }
+    if (y1 > y2) {
+      [y1, y2] = [y2, y1];
+    }
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        this.selected.add(i, j);
+      }
+    }
+  }
+
   private alignment(value: number | string) {
     if (typeof value === "number") return 'right';
     else return 'left';
+  }
+
+  private beginSelect(event: MouseEvent, i: number, j: number) {
+    event.preventDefault();
+    if (event.ctrlKey) {
+      this.selected.clear();
+      return;
+    }
+    this.selected.clear();
+    this._isSelecting = true;
+    this.selected.add(i, j);
+    this._startX = i;
+    this._startY = j;
+    document.addEventListener('mouseup', () => {
+      this._isSelecting = false;
+      document.removeEventListener('mouseup');
+    })
+  }
+
+  private onHover(event: MouseEvent, i: number, j: number) {
+    if (this._isSelecting) {
+      this.fillSelection(this._startX, this._startY, i, j);
+    }
+  }
+
+  private isSelected(i: number, j: number) {
+    return this.selected.contains(i, j);
   }
 }
 
@@ -132,7 +211,6 @@ export class HeaderCellComponent implements OnInit {
     if (this.top) {
       this.letters = this.toLetters(this.index)
     }
-    console.log(this)
   }
 
   public toLetters(num: number): string {
